7510392715d59b945d60b89cdd9f30a6af0b55ed
Ben Martin <1350682+ironstrider@users.noreply.github.com>
2022-12-13 16:51:55 +1000

    Add command palette for nav (WIP)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import './App.css'
 import QgisQuery from './QgisQuery'
 import AreaSeparator from './AreaSeparator'
+import CommandPalette from './CommandPalette'
+
+const tools = [
+  {
+    name: "QGis Query Builder",
+    id: "qgis-query-builder",
+    description: "lorem ipsum",
+  },
+  {
+    name: "Area Separator",
+    id: "area-separator",
+    description: "lorem dolor sit amet ipsum ",
+  },
+  {
+    name: "QGis Query Builder2",
+    id: "qgis-query-builder2",
+    description: "ipsum lorem validatum",
+  },
+
+];
 
 function App() {
   return (
     <div className="App">
+      <CommandPalette items={tools} onActivate={toolId => {
+        console.log('TODO: navigating to', toolId);
+      }} />
+      <hr className="mt-24" />
       <AreaSeparator />
-      {/* <hr className="my-8 border-1 border-slate-200"/>
-      <QgisQuery /> */}
+      <hr className="my-8 border-1 border-slate-200" />
+      <QgisQuery />
     </div>
   )
 }
---
+++ src/CommandPalette.tsx
@@ -0,0 +1,125 @@
+import { Combobox, Dialog, Transition } from "@headlessui/react"
+import { ChangeEvent, Fragment, useEffect, useState } from "react"
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
+interface CommandPaletteItem {
+  id: string,
+  name: string,
+  description?: string
+}
+
+function simpleFilter<T extends CommandPaletteItem>(items: T[], query?: string) {
+  if (!query) {
+    return items;
+  }
+
+  const normalisedQuery = query.toLowerCase();
+
+  return items.filter(tool => {
+    return tool.name.toLowerCase().includes(normalisedQuery) || tool.description?.includes(normalisedQuery)
+  })
+}
+
+export default function CommandPalette<T extends CommandPaletteItem>({
+  items,
+  onActivate,
+}: {
+  items: T[],
+  onActivate: (itemId: CommandPaletteItem["id"]) => void
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [query, setQuery] = useState('');
+
+  const filteredItems = simpleFilter(items, query);
+
+  useEffect(() => {
+    function onKeydown(event: any) {
+      if (event.key === 'k' && (event.metaKey || event.ctrlKey)) {
+        event.preventDefault();
+        setIsOpen(v => !v);
+      }
+    }
+    window.addEventListener('keydown', onKeydown);
+    return () => {
+      window.removeEventListener('keydown', onKeydown)
+    }
+  }, [setIsOpen])
+
+  return <Transition
+    show={isOpen}
+    as={Fragment}
+    afterLeave={() => {
+      setQuery("");
+    }}
+  >
+    <Dialog
+      onClose={setIsOpen}
+      className="fixed inset-0 p-4 pt-[25vh] overflow-y-auto"
+    >
+      <Transition.Child
+        className="fixed inset-0 bg-sky-200/75"
+        enter="duration-300 ease-out"
+        enterFrom="opacity-0 backdrop-blur-none"
+        enterTo="opacity-100 backdrop-blur-sm"
+        leave="duration-200 ease-in"
+        leaveFrom="opacity-100 backdrop-blur-sm"
+        leaveTo="opacity-0 backdrop-blur-none"
+      >
+        <Dialog.Overlay />
+      </Transition.Child>
+
+      <Transition.Child
+        enter="duration-300 ease-out"
+        enterFrom="opacity-0 scale-95"
+        enterTo="opacity-100 scale-100"
+        leave="duration-200 ease-in"
+        leaveFrom="opacity-100 scale-100"
+        leaveTo="opacity-0 scale-95"
+      >
+        <Combobox
+          as="div"
+          onChange={(value: string) => {
+            setIsOpen(false);
+            onActivate(value);
+          }}
+          className="relative max-w-xl mx-auto bg-white rounded-xl shadow-2xl ring-1 ring-black/5 divide-y divide-slate-200 overflow-hidden"
+          value={query}
+        >
+          <div className="flex items-center px-4">
+            <MagnifyingGlassIcon
+              className="h-6 w-6 text-slate-500"
+            />
+            <Combobox.Input
+              className="w-full border-0 bg-transparent text-sm focus:ring-0 text-slate-800 placeholder-slate-400"
+              placeholder="Search..."
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                setQuery(e.target.value);
+              }}
+            />
+          </div>
+          {filteredItems.length > 0 && (
+            <Combobox.Options static
+              className="py-4 text-sm max-h-96 overflow-y-auto"
+            >
+              {filteredItems.map(item => (
+                <Combobox.Option key={item.id} value={item.id}>
+                  {({ active }) => (
+                    <div className={`px-4 py-2 space-x-1 ${active ? 'bg-sky-600' : 'bg-white'}`}>
+                      <span className={`font-bold ${active ? 'text-white' : 'text-slate-800'} `}>{item.name}</span>
+                      <span className={active ? 'text-sky-200' : 'text-slate-400'}>{item.description}</span>
+                    </div>
+                  )}
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          )}
+          {
+            query && filteredItems.length === 0 && (
+              <p className="p-4 text-sm text-slate-500">No results found</p>
+            )
+          }
+        </Combobox>
+      </Transition.Child>
+    </Dialog>
+  </Transition>
+}
